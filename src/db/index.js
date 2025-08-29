9d1c43b836543fd2f1c8becdcfc918b9f9a93f7b
Dinkar <dkcan00@gmail.com>
2025-08-29 21:33:44 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose  from "mongoose";
+ import {DB_name} from '../constants.js';
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
+        console.log(`MongoDB connection established. DB host:${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Error in mongoDB connection",error)
+        process.exit(1)  
+      }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+})
+connectDB()
+
+
+
+
+
+
+
+
+// import mongoose from 'mongoose'
+
+// import { DB_name } from './constants';
+// import express from 'express'
+// const app = express()
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
+//         app.on("error",(error)=>{
+//             console.log("ERROR", error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app listening on ${process.env/PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("Error",error)
+//         throw errror
+//     }
+// })()
