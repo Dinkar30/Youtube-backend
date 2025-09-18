a6d64417d7e3039b09f67cea58fd3fd5c29dafdc
Dinkar30 <dkcan00@gmail.com>
2025-09-18 20:38:59 +0530

    add structural changes

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 import mongoose  from "mongoose";
  import {DB_name} from '../constants.js';
-const connectDB = async () => {
+const connectDB = async () => { 
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
         console.log(`MongoDB connection established. DB host:${connectionInstance.connection.host}`);
