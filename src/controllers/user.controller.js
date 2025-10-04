2faad00ab327593f955df4448ccd84721d1ab9dc
Dinkar30 <dkcan00@gmail.com>
2025-10-04 23:42:01 +0530

    add getWatchHistory functionality and update user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -285,7 +285,7 @@ const getUserChannelProfile = asyncHandler(async (req,res) => {
     const channel = await User.aggregate( [
     {
       $match: {
-        username: username
+        username: username?.toLowerCase()
       }
     },
     {
@@ -345,6 +345,58 @@ const getUserChannelProfile = asyncHandler(async (req,res) => {
 
 })
 
+
+const getWatchHistory = asyncHandler(async (req,res) => {
+     const user = await User.aggregate(
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from : "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+
+                                {$project: {
+                                    fullName: 1,
+                                    username: 1,
+                                    avatar: 1
+                                }}
+                            ]
+                        }
+                    },
+                    {
+                            $addFields: {
+                                owner: {
+                                    $first: "$owner"
+                                }
+                            }
+                        }
+                ]                 
+            }
+        }
+     )
+
+     return res
+            .status(200)
+            .json(200,
+                user[0].watchHistory,
+                "watch history fetched successfully"
+            )
+     
+})
+
 export { registerUser,
     loginUser,
     logoutUser,
@@ -353,6 +405,8 @@ export { registerUser,
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUsercoverImage
+    updateUsercoverImage,
+    getUserChannelProfile,
+    getWatchHistory
     
  }
