568e7cba313be61110e2e2ecff3864513fa45ce3
Dinkar30 <dkcan00@gmail.com>
2025-10-06 03:37:41 +0530

    bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -34,7 +34,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar)
-router.route("/cover-Image").patch(verifyJWT, upload.single("/coverImage"), updateUsercoverImage)
+router.route("/cover-Image").patch(verifyJWT, upload.single("coverImage"), updateUsercoverImage)
 
 
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
